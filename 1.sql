593c72d7525ef2262b4d2826caadb65d6b8d5d25
水饺 <126674519+suicao01@users.noreply.github.com>
2023-12-12 00:08:06 +0700

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,3 +1,4 @@
+//cau 1 & 2
 select c.customerName, c.customerNumber, (select datediff('2005-06-02',max(orderDate))
 from orders where customerNumber = c.customerNumber) as recency,
 (select count(*) from orders where customerNumber = c.customerNumber) as frequency,
@@ -7,3 +8,10 @@ from orders where customerNumber = c.customerNumber)) as monetary
 from customers c
 group by c.customerNumber
 having (recency <= 5 and frequency >=5) or monetary >= 500000
+
+//cau 3
+select pl.productLine, pl.textDescription, (select sum(quantityInStock +(select sum(quantityOrdered)
+from orderdetails where productCode = p.productCode))
+from products p where p.productLine =pl.productLine) as totalProducts
+from productlines pl
+group by pl.productLine
